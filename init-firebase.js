5c2f9bbe356be03b37f350d2e86d9b441a86e89c
RobbenHsieh <robbenhsieh@gmail.com>
2020-12-22 16:20:44 +0800

    file upload

---
+++ init-firebase.js
@@ -0,0 +1,12 @@
+// TODO: Replace the following with your app's Firebase project configuration
+
+var firebaseConfig = {
+    apiKey: "<insert your API Key>",
+    authDomain: "<project id>.firebaseapp.com",
+    databaseURL: "https://<project id>-default-rtdb.firebaseio.com/",
+    projectId: "<project id>",
+    storageBucket: "gs://<project id>.appspot.com/",
+};
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
